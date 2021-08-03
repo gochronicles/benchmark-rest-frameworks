8dda33683f9893f040a64ccb2fe9f62c878c966c
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2021-08-03 07:37:13 +0100

    multiple-workers enabled for py

--- rest-fastapi-python/app.py
+++ rest-fastapi-python/app.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,9 @@ app = FastAPI()
 @app.get("/")
 async def hello():
     return "Hello World"
+
+
+if __name__ == "__main__":
+    workers = os.cpu_count()
+    print(f"Number of CPUs: {workers}")
+    uvicorn.run(app, host="0.0.0.0", port=3000, workers=workers, log_level="critical")
