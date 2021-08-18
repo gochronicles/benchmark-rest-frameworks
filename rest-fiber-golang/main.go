cdf89eed5d0e89731c9eed9086cb970662eba744
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2021-08-18 20:45:23 +0530

    prefork enabled

--- rest-fiber-golang/main.go
+++ rest-fiber-golang/main.go
@@ -3,12 +3,14 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-    app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork:      true,
+		ServerHeader: "Fiber",
+		AppName:      "Benchmark App",
+	})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
-
-    app.Listen(":3000")
+	app.Listen(":3000")
 }
-
